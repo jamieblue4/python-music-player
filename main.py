2b1cdb1eaf2d412661e75d14ca597bb2b29d4534
Jamie Gist <jamiegist@macbook-pro.mynetworksettings.com>
2024-06-09 20:32:01 -0700

    Plays local audio files. Need to fix issue where listbox doesn't render in the app.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from tkinter import filedialog
 from tkinter import *
 import pygame
 import os
@@ -12,10 +13,64 @@ pygame.mixer.init()
 menubar = Menu(root)
 root.config(menu=menubar)
 
-organize_menu = Menu(menubar, tearoff=False)
-organize_menu.add_command(label='Select Folder')
-menubar.add_cascade(label='Organize', menu=organize_menu)
+songs = []
+current_song = ""
+paused = False
 
+def load_music():
+    global current_song
+    root.directory = filedialog.askdirectory()
+
+    for song in os.listdir(root.directory):
+        name, ext = os.path.splitext(song)
+        if ext == '.mp3':
+            songs.append(song)
+
+        for song in songs:
+            songlist.insert("end", song)
+
+        songlist.selection_set(0)
+        current_song = songs[songlist.curselection()[0]]
+
+def play_music():
+    global current_song, paused
+    
+    if not paused:
+        pygame.mixer.music.load(os.path.join(root.directory, current_song))
+        pygame.mixer.music.play()
+    else:
+        pygame.mixer.music.unpause()
+        paused = False
+
+def pause_music():
+    global pause
+    pygame.mixer.music.pause()
+    paused = True
+
+def next_music():
+    global current_song, paused
+    try:
+        songlist.selection_clear(0, END)
+        songlist.selection_set(songs.index(current_song) + 1)
+        current_song = songs[songlist.curselection()[0]]
+        play_music()
+    except:
+        pass
+
+def prev_music():
+    global current_song, paused
+
+    try:
+        songlist.select_clear(0, END)
+        songlist.selection_set(songs.index(current_song) - 1)
+        current_song = songs[songlist.curselection()[0]]
+        play_music()
+    except:
+        pass
+
+organize_menu = Menu(menubar, tearoff=False)
+organize_menu.add_command(label='Select Folder', command=load_music)
+menubar.add_cascade(label='Organize', menu=organize_menu)
 
 songlist = Listbox(root, bg="black", fg="white", width=100, height=15)
 songlist.pack()
@@ -28,10 +83,10 @@ back_btn_img = ImageTk.PhotoImage(Image.open("back.png"))
 control_frame = Frame(root)
 control_frame.pack()
 
-play_btn = Button(control_frame, image=play_btn_img, borderwidth=0)
-pause_btn = Button(control_frame, image=pause_btn_img, borderwidth=0)
-next_btn = Button(control_frame, image=next_btn_img, borderwidth=0)
-back_btn = Button(control_frame, image=back_btn_img, borderwidth=0)
+play_btn = Button(control_frame, image=play_btn_img, borderwidth=0, command=play_music)
+pause_btn = Button(control_frame, image=pause_btn_img, borderwidth=0, command=pause_music)
+next_btn = Button(control_frame, image=next_btn_img, borderwidth=0, command=next_music)
+back_btn = Button(control_frame, image=back_btn_img, borderwidth=0, command=prev_music)
 
 play_btn.grid(row=0, column=1, padx=7, pady=10)
 pause_btn.grid(row=0, column=2, padx=7, pady=10)
